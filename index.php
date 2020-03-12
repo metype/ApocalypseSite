8d4b2cbf809ab3c5a6cc06ac6bbc82a0d1cff475
Metype <39036543+metype@users.noreply.github.com>
2020-03-12 02:07:02 -0500

    Fix footer

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ var description=<?php echo json_encode(file("description.txt"));?>;
         <title class="title"></title>
     </head>
     <body>
+        <div class="footer">The Apocalypse: First Blood is owned and maintained by NWG Games with minor assistance from 1Maker Stduios, please do not redistribute copies of "The Apocalypse: First Blood" or create any modifications to the game that bypass restrctions against playing multiplayer with said clients. Thank you.</div>
         <nav class='navbar'>
             <ul class="navbar-nav">
                 <li class="logo">
@@ -45,6 +46,5 @@ var description=<?php echo json_encode(file("description.txt"));?>;
         </nav>
         <div class="main"></div>
         <script><?php include 'index.js'; ?></script>
-        <div class="footer">The Apocalypse: First Blood is owned and maintained by NWG Games with minor assistance from 1Maker Stduios, please do not redistribute copies of "The Apocalypse: First Blood" or create any modifications to the game that bypass restrctions against playing multiplayer with said clients. Thank you.</div>
     </body>
 </html>
