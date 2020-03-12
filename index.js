f28d435a31a6b4b5d6205d0bae7b6e976a98e661
Metype <39036543+metype@users.noreply.github.com>
2020-03-11 19:23:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ function errorHandler(errnum, err) {
 }
 
 function home() {
-    $('<div/>').addClass('info').appendTo($('div.main'));
-    $('<h3/>').text("The Apocalypse: First Blood").attr('style', 'height:1000').appendTo("div.info");
-    $('<p/>').text(description).addClass("homePageParagraph1").appendTo('div.info');
+    $('<div/>').addClass('info').attr('style', 'height:1000').appendTo($('div.main'));
+    $('<h3/>').text("The Apocalypse: First Blood").appendTo("div.info");
+    $('<p/>').text(description).addClass("homePageParagraph1").attr('style', 'font-size:10').appendTo('div.info');
 }
 
 function contact() {
